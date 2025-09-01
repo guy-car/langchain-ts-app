5102d2cd5b96a9c4b2291efb40eda31114a387a1
guy-car <guil.crn@gmail.com>
2025-09-01 12:31:25 -0400

    basic chat

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-'hello world';
+import 'dotenv/config';
+import { ChatOpenAI } from '@langchain/openai';
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
 
-console.log('hello world');
+const model = new ChatOpenAI({ model: 'gpt-5-chat-latest' });
+
+const messages = [
+  new SystemMessage('Translate the following from English into French'),
+  new HumanMessage("hi! I'm fat and ugly, can you help me?"),
+];
+
+const response = await model.invoke(messages);
+console.log(response);

8c08429f8a8ef83438c86902cea9429c24906906
guy-car <guil.crn@gmail.com>
2025-09-01 15:02:09 -0400

    simple stream translater

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { ChatOpenAI } from '@langchain/openai';
 import { HumanMessage, SystemMessage } from '@langchain/core/messages';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
 
 const model = new ChatOpenAI({ model: 'gpt-5-chat-latest' });
 
@@ -9,5 +10,30 @@ const messages = [
   new HumanMessage("hi! I'm fat and ugly, can you help me?"),
 ];
 
-const response = await model.invoke(messages);
-console.log(response);
+// const response = await model.invoke(messages);
+// console.log(response);
+
+// const stream = await model.stream(messages);
+
+// const chunks = [];
+// for await (const chunk of stream) {
+//   chunks.push(chunk);
+//   console.log(`${chunk.content}|`);
+// }
+
+const systemTemplate = 'translate the following into {language}';
+
+const promptTemplate = ChatPromptTemplate.fromMessages([
+  ['system', systemTemplate],
+  ['user', '{text}'],
+]);
+
+const promptValue = await promptTemplate.invoke({
+  language: 'French',
+  text: 'What are we eating for lunch?',
+});
+
+// console.log(promptValue);
+
+const response = await model.invoke(promptValue);
+console.log(`🤖AI Reponse: ${response.content}`);
